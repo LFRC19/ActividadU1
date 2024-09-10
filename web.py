e20d3f9a82a3ff5610eef0bdafb08b8321ccec0f
Luis Fernando Rodriguez Cruz <92768344+LFRC19@users.noreply.github.com>
2024-09-10 17:42:29 +0000

    Capturando de headers Host y User-Agent, respuesta con Content-Type, Server y Date

--- web.py
+++ web.py
@@ -10,18 +10,33 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
+    # Recoger los valores de los headers del request
+    host = self.headers.get('Host')
+    user_agent = self.headers.get('User-Agent')
 
-    def get_response(self):
-        return f"""
+    # Responder con un código 200
+    self.send_response(200)
+
+    # Agregar los headers de la respuesta
+    self.send_header("Content-Type", "text/html")
+    self.send_header("Server", "CustomPythonServer")
+    self.send_header("Date", self.date_time_string())
+    self.end_headers()
+
+    # Escribir la respuesta en el cuerpo
+    self.wfile.write(self.get_response(host, user_agent).encode("utf-8"))
+
+def get_response(self, host, user_agent):
+    return f"""
     <h1> Hola Web </h1>
-    <p> URL Parse Result : {self.url()}         </p>
-    <p> Path Original: {self.path}         </p>
-    <p> Headers: {self.headers}      </p>
-    <p> Query: {self.query_data()}   </p>
+    <h1> Made by Luis Fernando Rodriguez Cruz </h1>
+    <p> URL Parse Result : {self.url()} </p>
+    <p> Path Original: {self.path} </p>
+    <p> Headers: {self.headers} </p>
+    <p> Query: {self.query_data()} </p>
+    <p> Host: {host} </p>
+    <p> User-Agent: {user_agent} </p>
+    """
 """
 
 
